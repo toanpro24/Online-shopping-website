424c07bbee2b97765a58b9638dca884a0f51617e
Your Name <you@example.com>
2023-08-03 15:26:28 +0700

    modified:   admin.js
    Changes not staged for commit:
    	modified:   admin.css
    	modified:   admin.py
    	modified:   admin_product_table.html
    
    Untracked files:
    	admin_product_table.css
    	admin_product_table_redirect.html

--- admin.js
+++ admin.js
@@ -1,37 +1,158 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const ProductIDSearch = document.getElementById("ProductID");
-    const CategoryIDSearch = document.getElementById('CategoryID');
-    const ProductNameSearch= document.getElementById('ProductName');
-    
+    const productIDSearch = document.getElementById('ProductID');
+    const productNameSearch = document.getElementById('ProductName');
+    const categoryIDSearch = document.getElementById('CategoryID');
+
     function performSearch(column, searchTerm) {
-        data = {}
-        data['col'] = column
-        data['searchterm'] = searchTerm
-        fetch('/search', {
+        data = {};
+        data['column'] = column;
+        data['searchTerm'] = searchTerm;
+        // fetch("/search", {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json', 
+        //     },
+        //     body: JSON.stringify(data),
+        // });
+        const queryString = encodeURIComponent(JSON.stringify(data));
+
+        // Make the GET request with JSON data
+        fetch(`/search?data=${queryString}`, {
+            method: 'GET',
+        });
+        location.href = `/search?data=${queryString}`;
+    }
+
+    function handleEnterKey(event, column) {
+        if (event.key === 'Enter') {
+            const searchTerm = event.target.value;
+            performSearch(column, searchTerm);
+        }
+    }
+    
+    if (productIDSearch) {
+        productIDSearch.addEventListener('keydown', event => handleEnterKey(event, 'ProductID'));
+    }
+    
+    if (productNameSearch) {
+        productNameSearch.addEventListener('keydown', event => handleEnterKey(event, 'ProductName'));
+    }
+    
+    if (categoryIDSearch) {
+        categoryIDSearch.addEventListener('keydown', event => handleEnterKey(event, 'CategoryID'));
+    }
+});
+document.addEventListener('DOMContentLoaded', function() {
+    const productIDSearch = document.getElementById('ProductID');
+    const productNameSearch = document.getElementById('ProductName');
+    const categoryIDSearch = document.getElementById('CategoryID');
+
+    function performSearch(column, searchTerm) {
+        data = {};
+        data['column'] = column;
+        data['searchTerm'] = searchTerm;
+        // fetch("/search", {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json', 
+        //     },
+        //     body: JSON.stringify(data),
+        // });
+        const queryString = encodeURIComponent(JSON.stringify(data));
+
+        // Make the GET request with JSON data
+        fetch(`/search?data=${queryString}`, {
+            method: 'GET',
+        });
+        location.href = `/search?data=${queryString}`;
+    }
+
+    function handleEnterKey(event, column) {
+        if (event.key === 'Enter') {
+            const searchTerm = event.target.value;
+            performSearch(column, searchTerm);
+        }
+    }
+    
+    if (productIDSearch) {
+        productIDSearch.addEventListener('keydown', event => handleEnterKey(event, 'ProductID'));
+    }
+    
+    if (productNameSearch) {
+        productNameSearch.addEventListener('keydown', event => handleEnterKey(event, 'ProductName'));
+    }
+    
+    if (categoryIDSearch) {
+        categoryIDSearch.addEventListener('keydown', event => handleEnterKey(event, 'CategoryID'));
+    }
+});
+document.addEventListener('DOMContentLoaded', function() {
+    const editButtons = document.querySelectorAll('.edit-button');
+
+    function handleEditClick(event) {
+        const row = event.target.closest('tr');
+        const cells = row.querySelectorAll('td');
+        
+        for (let i = 0; i < cells.length - 1; i++) {
+            if(i == 1){
+                continue;
+            }
+            const cell = cells[i];
+            const cellContent = cell.textContent;
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.value = cellContent;
+            cell.textContent = '';
+            cell.appendChild(input);
+        }
+
+        event.target.remove();
+
+        const saveButton = document.createElement('button');
+        saveButton.className = 'save-button';
+        saveButton.textContent = 'Save';
+        saveButton.addEventListener('click', function() {
+            handleSaveClick(row);
+        });
+        row.querySelector('td:last-child').appendChild(saveButton);
+    }
+
+    function handleSaveClick(row) {
+        const inputs = row.querySelectorAll('input');
+        const data = {};
+        for (let i = 0; i < inputs.length; i++) {
+            const input = inputs[i];
+            const columnName = input.parentElement.getAttribute('data-column-name');
+            const inputValue = input.value;
+            data[columnName] = inputValue;
+        }
+
+        fetch('/update', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(data),
         })
-        .then(response => response.json())
-        .catch(error => console.error('Error:', error));
-        location.reload();
+       
+        for (let i = 0; i < inputs.length; i++) {
+            const input = inputs[i];
+            const inputValue = input.value;
+            const cell = input.parentElement;
+            cell.textContent = inputValue;
+        }
+
+        const editButton = document.createElement('button');
+        editButton.className = 'edit-button';
+        editButton.textContent = 'Edit';
+        editButton.addEventListener('click', handleEditClick(row));
+        row.querySelector('td:last-child').appendChild(editButton);          
+        row.querySelector('.save-button').remove();
     }
 
-
-    ProductIDSearch.addEventListener('keyup', function(event) {
-        const searchTerm = event.target.value;
-        performSearch('ProductID', searchTerm);
-    });
-
-    CategoryIDSearch.addEventListener('keyup', function(event) {
-        const searchTerm = event.target.value;
-        performSearch('CategoryID', searchTerm);
-    });
-
-    ProductNameSearch.addEventListener('keyup', function(event) {
-        const searchTerm = event.target.value;
-        performSearch('Product name', searchTerm);
+    editButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            handleEditClick(button);
+        });
     });
 });
